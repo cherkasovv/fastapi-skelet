9e14b821f8c83901d10e2b96a532de92400eae4c
cherkasovv <cherkasovv.evgeny@gmail.com>
2022-03-15 21:11:10 +0300

    asdfasdf

--- {{cookiecutter.project_slug}}/app/app/core/config.py
+++ {{cookiecutter.project_slug}}/app/app/core/config.py
@@ -6,8 +6,6 @@ from pydantic import AnyHttpUrl, BaseSettings, EmailStr, HttpUrl, PostgresDsn, v
 
 class Settings(BaseSettings):
     API_V1_STR: str = "/api/v1"
-    SERVER_NAME: str
-    SERVER_HOST: AnyHttpUrl
     # BACKEND_CORS_ORIGINS is a JSON-formatted list of origins
     # e.g: '["http://localhost", "http://localhost:4200", "http://localhost:3000", \
     # "http://localhost:8080", "http://local.dockertoolbox.tiangolo.com"]'
