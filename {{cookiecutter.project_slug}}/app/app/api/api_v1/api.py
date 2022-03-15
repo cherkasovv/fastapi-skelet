d8ecbdeab445806c052a870a3416ed4d3b86dac7
cherkasovv <cherkasovv.evgeny@gmail.com>
2022-03-15 20:19:58 +0300

    a

--- {{cookiecutter.project_slug}}/app/app/api/api_v1/api.py
+++ {{cookiecutter.project_slug}}/app/app/api/api_v1/api.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-from app.api.api_v1.endpoints import items, login, users, utils
+from app.api.api_v1.endpoints import example
 
 api_router = APIRouter()
-api_router.include_router(users.router, prefix="/example", tags=["example"])
+api_router.include_router(example.router, prefix="/example", tags=["example"])
